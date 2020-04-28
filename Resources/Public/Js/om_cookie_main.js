f276645f7188f531a9116715dd9d21a11d8f2ec7
Oliver Pfaff <info@olli-machts.de>
2020-04-28 11:42:51 +0200

    ADD SameSite attribute

--- Resources/Public/Js/om_cookie_main.js
+++ Resources/Public/Js/om_cookie_main.js
@@ -193,7 +193,7 @@ var omCookieUtility = {
     setCookie: function(name, value, days) {
             var d = new Date;
             d.setTime(d.getTime() + 24*60*60*1000*days);
-            document.cookie = name + "=" + value + ";path=/;expires=" + d.toGMTString();
+            document.cookie = name + "=" + value + ";path=/;expires=" + d.toGMTString() + ";SameSite=Lax";
         },
     deleteCookie: function(name){ setCookie(name, '', -1); }
 };
