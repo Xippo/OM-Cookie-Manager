7bafa5fa2bda5c48d5dde68e8eb4e5bc6f314570
Oliver Pfaff <info@olli-machts.de>
2020-04-28 11:43:53 +0200

    ADD TYPO3 10 compatibility

--- ext_emconf.php
+++ ext_emconf.php
@@ -11,10 +11,10 @@ $EM_CONF[$_EXTKEY] = [
     'uploadfolder' => 0,
     'createDirs' => '',
     'clearCacheOnLoad' => 0,
-    'version' => '9.2.1',
+    'version' => '10.0.0',
     'constraints' => [
         'depends' => [
-            'typo3' => '7.6.0-9.5.99',
+            'typo3' => '7.6.0-10.4.99',
         ],
         'conflicts' => [],
         'suggests' => [],
