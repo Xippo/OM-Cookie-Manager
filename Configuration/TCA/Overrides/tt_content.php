3a351685b4bf4e26e3efaf0ddf4504fdd1e15d04
Lukas Niestroj <niestrojlukas@gmail.com>
2022-03-10 11:23:21 +0100

    [TASK] move addStaticFile to TCA/Overrides/sys_template.php, move registerPlugin to TCA/Overrides/tt_content.php
    Fixes an issue during "typo3cms upgrade:prepare" with typo3_console:^7 and TYPO3 ^11.5. typo3_console will try to load the ext_tables.php but this will result in "Warning: Undefined global variable $TCA in [...]/typo3/sysext/core/Classes/Utility/ExtensionManagementUtility.php line 1225". And this results ultimately into a different exception like " The package "X" depends on "Y" which is not present in the system."

---
+++ Configuration/TCA/Overrides/tt_content.php
@@ -0,0 +1,8 @@
+<?php
+defined('TYPO3_MODE') || die('Access denied.');
+
+\TYPO3\CMS\Extbase\Utility\ExtensionUtility::registerPlugin(
+    'OM.OmCookieManager',
+    'Main',
+    'OM Cookie Manager'
+);
