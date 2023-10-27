62f1b6293a8bb24b0a7fcbc2b0ca729fe067ad98
DerBasti <baschte@users.noreply.github.com>
2023-10-27 11:29:55 +0200

    [BUGFIX] update essential tca

--- Configuration/TCA/tx_omcookiemanager_domain_model_cookiegroup.php
+++ Configuration/TCA/tx_omcookiemanager_domain_model_cookiegroup.php
@@ -158,9 +158,7 @@ return [
             'config' => [
                 'type' => 'check',
                 'items' => [
-                    '1' => [
-                        '0' => 'LLL:EXT:lang/locallang_core.xlf:labels.enabled'
-                    ]
+                    ['LLL:EXT:lang/locallang_core.xlf:labels.enabled'],
                 ],
                 'default' => 0,
             ]
