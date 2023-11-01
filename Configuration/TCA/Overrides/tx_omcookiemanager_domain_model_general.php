6bfdd1b0f05f737aad3b953911d7ff80e4cf02b9
Xippo <oliver.pfaff.email@gmail.com>
2023-11-01 16:58:22 +0100

    CHANGE set new ignorePageTypeRestriction inside override

---
+++ Configuration/TCA/Overrides/tx_omcookiemanager_domain_model_general.php
@@ -0,0 +1,6 @@
+<?php
+
+$GLOBALS['TCA']['tx_omcookiemanager_domain_model_cookie']['ctrl']['security']['ignorePageTypeRestriction'] = true;
+$GLOBALS['TCA']['tx_omcookiemanager_domain_model_cookiegroup']['ctrl']['security']['ignorePageTypeRestriction'] = true;
+$GLOBALS['TCA']['tx_omcookiemanager_domain_model_cookiepanel']['ctrl']['security']['ignorePageTypeRestriction'] = true;
+$GLOBALS['TCA']['tx_omcookiemanager_domain_model_cookiehtml']['ctrl']['security']['ignorePageTypeRestriction'] = true;
