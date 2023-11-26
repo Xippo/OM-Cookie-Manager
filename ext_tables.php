22481e51d84433584d4c297c4e7b51de91c0c3e9
Oliver Pfaff <info@olli-machts.de>
2023-11-26 17:13:01 +0100

    CHANGE remove unused Context Sensitive Help, fix extension scanner add TODO for migration for TYPO3 13

--- ext_tables.php
+++ ext_tables.php
@@ -2,12 +2,12 @@
 
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPageTSConfig('mod.web_list.hideTables = tx_omcookiemanager_domain_model_cookie,tx_omcookiemanager_domain_model_cookiehtml' . PHP_EOL . ' mod.web_list.deniedNewTables = tx_omcookiemanager_domain_model_cookie,tx_omcookiemanager_domain_model_cookiehtml');
 
-//compatibility to 11
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addLLrefForTCAdescr('tx_omcookiemanager_domain_model_cookiehtml', 'EXT:om_cookie_manager/Resources/Private/Language/locallang_csh_tx_omcookiemanager_domain_model_cookiehtml.xlf');
+//compatibility to TYPO3 11 @todo migrate in TYPO3 13
+// @extensionScannerIgnoreLine
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::allowTableOnStandardPages('tx_omcookiemanager_domain_model_cookiehtml');
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addLLrefForTCAdescr('tx_omcookiemanager_domain_model_cookie', 'EXT:om_cookie_manager/Resources/Private/Language/locallang_csh_tx_omcookiemanager_domain_model_cookie.xlf');
+// @extensionScannerIgnoreLine
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::allowTableOnStandardPages('tx_omcookiemanager_domain_model_cookie');
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addLLrefForTCAdescr('tx_omcookiemanager_domain_model_cookiegroup', 'EXT:om_cookie_manager/Resources/Private/Language/locallang_csh_tx_omcookiemanager_domain_model_cookiegroup.xlf');
+// @extensionScannerIgnoreLine
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::allowTableOnStandardPages('tx_omcookiemanager_domain_model_cookiegroup');
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addLLrefForTCAdescr('tx_omcookiemanager_domain_model_cookiepanel', 'EXT:om_cookie_manager/Resources/Private/Language/locallang_csh_tx_omcookiemanager_domain_model_cookiepanel.xlf');
+// @extensionScannerIgnoreLine
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::allowTableOnStandardPages('tx_omcookiemanager_domain_model_cookiepanel');
