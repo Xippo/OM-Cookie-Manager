e217c3c9bc2a7dda7e96cce497ddcd4b1dab818c
Xippo <oliver.pfaff.email@gmail.com>
2025-04-07 15:49:01 +0200

    CHANGE migrate and remove call of deprecated API

--- ext_tables.php
+++
@@ -1,3 +0,0 @@
-<?php
-
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPageTSConfig('mod.web_list.hideTables = tx_omcookiemanager_domain_model_cookie,tx_omcookiemanager_domain_model_cookiehtml' . PHP_EOL . ' mod.web_list.deniedNewTables = tx_omcookiemanager_domain_model_cookie,tx_omcookiemanager_domain_model_cookiehtml');
